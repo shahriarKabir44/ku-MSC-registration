0e53027306453521b6fdd264eaca533b28c98695
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-06-09 12:28:35 +0600

    updated styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,23 +22,27 @@
 				<h3 class="inputGroupHeader">Personal Information</h3>
 				<div class="gridContainer">
 						<label for="name"> Applicant's name</label>
-						<input required name="name" type="text" />
+						<input class="form-control" required name="name" type="text" />
 						<label for="fatherName"> Father's name</label>
-						<input required name="fatherName" type="text" />
+						<input class="form-control" required name="fatherName" type="text" />
 						<label for="motherName"> Mother's name</label>
-						<input required name="motherName" type="text" />
+						<input class="form-control" required name="motherName" type="text" />
 						<label for="gender">Gender</label>
-						<select required name="gender" id="">
+						<select  class="form-control" required name="gender" id="">
 							<option value="male">Male</option>
 							<option value="female">Female</option>
 						</select>
 						<label for="religion">Religion</label>
-						<select required name="religion" id="">
+						<select  class="form-control" required name="religion" id="">
 							<option value="Islam">Islam</option>
 							<option value="Hindu">Hindu</option>
 							<option value="Christianity">Christianity</option>
 							<option value="Buddhism">Buddhism</option>
 						</select>
+						<label for="email"> Email</label>
+						<input class="form-control" required name="email" type="email" />
+						<label for="phone"> Phone </label>
+						<input class="form-control" placeholder="(+88)" required name="phone" type="text" />
 				</div>	
 			
 			</div>
@@ -46,9 +50,9 @@
 				<h3 class="inputGroupHeader">Address</h3>
 				<div class="gridContainer">
 						<label for="permanentAddress">Permanent Address</label>
-						<input required name="permanentAddress" type="text" />
+						<input class="form-control" required name="permanentAddress" type="text" />
 						<label for="presentAddress">Present Address</label>
-						<input required name="presentAddress" type="text" />
+						<input class="form-control" required name="presentAddress" type="text" />
 				</div>
 				
 			</div>
@@ -57,17 +61,17 @@
 				<h3 class="inputGroupHeader">Birth Information</h3>
 				<div class="gridContainer">
 						<label for="birthDistrict">Birth district</label>
-						<input type="text" name="birthDistrict" id="">
+						<input class="form-control" type="text" name="birthDistrict" id="">
 						<label for="dateOfBirth">Date of birth</label>
-						<input type="date" name="birthDistrict" id="">
+						<input class="form-control" type="date" name="birthDistrict" id="">
 						<label for="nationality">Nationality</label>
-						<input type="text" name="nationality" id="">
+						<input class="form-control" type="text" name="nationality" id="">
 				</div>
 			</div>
 			
 			<div  class="inputGroup">
 				<h3 class="inputGroupHeader">Educational Qualification</h3>
-				<table border="1">
+				<table  class="table table-striped" >
 					<thead>
 					 
 						<tr class="formTableRow">
@@ -83,15 +87,15 @@
 								Passing year
 							</td>
 							<th>			
-								<input placeholder="Type here" class="tableInput" type="text"  name="bsc_passing_yr" id="">
+								<input placeholder="Type here" class="tableInput form-control" type="text"  name="bsc_passing_yr" id="">
 							</th>
 							<td>	
 							 			
-								<input placeholder="Type here" class="tableInput" type="text"   name="hsc_university_name" id="">
+								<input placeholder="Type here" class="tableInput form-control" type="text"   name="hsc_university_name" id="">
 							</td>
 							<td>	
 								 	
-								<input placeholder="Type here" class="tableInput" type="text"  name="ssc_university" id="">
+								<input placeholder="Type here" class="tableInput form-control" type="text"  name="ssc_university" id="">
 							</td>
 							
 						</tr>
@@ -99,26 +103,26 @@
 						<tr class="formTableRow">
 							<td>Board / university</td>
 							<td>				
-								<input placeholder="Type here" class="tableInput" type="text" name="bsc_university" id="">
+								<input placeholder="Type here" class="tableInput form-control" type="text" name="bsc_university" id="">
 							</td>
 							<td>				
-								<input placeholder="Type here" class="tableInput" type="text" name="hsc_board_name" id="">
+								<input placeholder="Type here" class="tableInput form-control" type="text" name="hsc_board_name" id="">
 							</td>
 							<td>				
-								<input placeholder="Type here" class="tableInput" type="text" name="ssc_board_name" id="">
+								<input placeholder="Type here" class="tableInput form-control" type="text" name="ssc_board_name" id="">
 							</td>
 							 
 						</tr>
 						<tr class="formTableRow">
 							<td>GPA</td>
 							<td>				
-								<input placeholder="Type here" class="tableInput" type="text" name="bsc_GPA" id="">
+								<input placeholder="Type here" class="tableInput form-control" type="text" name="bsc_GPA" id="">
 							</td>
 							<td>				
-								<input placeholder="Type here" class="tableInput" type="text" name="hsc_GPA" id="">
+								<input placeholder="Type here" class="tableInput form-control" type="text" name="hsc_GPA" id="">
 							</td>
 							<td>				
-								<input placeholder="Type here" class="tableInput" type="text" name="ssc_GPA" id="">
+								<input placeholder="Type here" class="tableInput form-control" type="text" name="ssc_GPA" id="">
 							</td>
 							 
 						</tr>
@@ -130,12 +134,25 @@
 				<h3 class="inputGroupHeader">Employment Status <span style="font-size: 15px">(Optional)</span></h3>
 				<div class="gridContainer">
 					<label for="companyName">Company / institute name</label>
-					<input type="text" name="companyName" id="">
-					<label for="companyPosition">Position</label>
-					<input type="text" name="companyPosition" id="">
-					<label for="joiningDate">Joining date</label>
-					<input type="date" name="joiningDate" id="">
+					<input class="form-control" type="text" name="companyName" id="">
+					<label    for="companyPosition">Position</label>
+					<input  class="form-control" type="text" name="companyPosition" id="">
+					<label   for="joiningDate">Joining date</label>
+					<input  class="form-control" type="date" name="joiningDate" id="">
 				</div>
+			</div>
+
+			<div class="inputGroup">
+				<h3 class="inputGroupHeader">Research <span style="font-size: 15px">(Optional)</span></h3>
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th>Proposed Research Field</th>
+							<th>Supevisor Name</th>
+							<th>Position of the Supervisor</th>
+						</tr>
+					</thead>
+				</table>
 			</div>
         </form>
     </body>
