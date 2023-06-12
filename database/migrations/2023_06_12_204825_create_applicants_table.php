2ba5e5ddf7751a79593dce5d5ec3bd5e0e702d53
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2023-06-13 02:52:23 +0600

    created model

--- database/migrations/2023_06_12_204825_create_applicants_table.php
+++ database/migrations/2023_06_12_204825_create_applicants_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,32 @@ return new class extends Migration
     {
         Schema::create('applicants', function (Blueprint $table) {
             $table->id();
+            $table->string('photo');
+            $table->string('name');
+            $table->string('fatherName');
+            $table->string('motherName');
+            $table->string('gender');
+            $table->string('religion');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('permanentAddress');
+            $table->string('birthDate');
+            $table->string('nationality');
+            $table->string('discipline');
+            $table->string('presentAddress');
+            $table->string('hons_passing_yr');
+            $table->string('hons_university');
+            $table->string('hons_GPA');
+            $table->string('hsc_GPA');
+            $table->string('hsc_board_name');
+            $table->string('hsc_passing_yr');
+            $table->string('ssc_passing_yr');
+            $table->string('ssc_board_name');
+            $table->string('ssc_GPA');
+            $table->string('companyName');
+            $table->string('companyPosition');
+            $table->string('joiningDate');
+
             $table->timestamps();
         });
     }
